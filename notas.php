930241b983fdf749f7646cdcf93bca722a151807
sebasandrefa <sebasandrefa@gmail.com>
2026-09-21 15:17:02 -0300

    .

--- notas.php
+++ notas.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Notas</title>
 </head>
 <body>
-    <a href="idade.php">verificador de idade</a>
-    <br>
-    <a href="notas.php">verificador de notas</a>
+    
 </body>
 </html>
