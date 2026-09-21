0a5c44738ef5f907370dbc6c7569e07d6b053dd2
sebasandrefa <sebasandrefa@gmail.com>
2026-09-21 17:12:36 -0300

    .
    login e senha, tentando

--- l.php
+++ l.php
@@ -1,29 +1,35 @@
+<?php
+$usuario = "";
+$senha = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro do aluno</title>
+    <title>Login</title>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <h1>Cadastro do aluno</h1>
+    <h1>Login</h1>
 
     <form method="POST">
-        <input type="text" name="nome" placeholder="Digite seu nome" required>
-        <input type="number" name="idade" placeholder="Digite sua idade" required>
+        <input type="text" name="usuario" placeholder="Usuário" required>
+        <input type="password" name="senha" placeholder="Senha" required>
         <button type="submit">Entrar</button>
     </form>
 
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_POST["nome"];
-        $idade = $_POST["idade"];
-
-        echo "<h2>Olá, $nome!</h2>";
-        echo "<p>Idade: $idade anos</p>";
-    }
-    ?>
+    <?php if ($usuario == "aluno" && $senha == "123") { ?>
+        <a href="idade.php">Idade</a>
+        <a href="notas.php">Notas</a>
+        <a href="notas-desafio.php">Desafio notas</a>
+    <?php } ?>
 
 </body>
 </html>
