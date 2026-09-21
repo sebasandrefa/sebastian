4e20c1d270020bb67031c08fb6165b5d46f3ab0d
sebasandrefa <sebasandrefa@gmail.com>
2026-09-21 15:01:32 -0300

    .

--- idade.php
+++ idade.php
@@ -1,11 +1,65 @@
+<?php
+
+$nome = "";
+
+$idade = 0;
+
+$mostrar = "";
+
+if ($_SERVER ["REQUEST_METHOD"] == "POST"){
+    
+    $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
+    
+    if ($idade >=18){
+        $mostrar = " De Maior";
+    }
+    else {
+        $mostrar = "De Menor";
+    }
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PT-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    
+    <title>project</title>
 </head>
 <body>
+
+
+<form method="POST">
+
+    <input type="text" id="nome" name="nome" placeholder="Digite seu Nome">
+
+    <input type="number" id="idade"  name="idade" placeholder="Digite sua idade">
+
+    <input type="submit" value="Enviar">
+    </form>
+
+    <div class="card">
+
+        
+        <h1>Mostrando Nome, Idade e resultado</h1>
+        
     
-</body>
-</html>
+    <?php if ($mostrar != "") { ?>
+        
+        <h2>
+            o <?= $nome ?> é <?= $mostrar ?> .
+        </h2>
+        
+        <?php } ?>
+        
+        <h1>O meu nome é <?=  $nome?> </h1>
+        <h2>Minha idade é <?=  $idade?> </h2>
+        <p>De acordo com a idade, eu sou <?= $mostrar ?></p>
+
+    </div>
+        
+    </body>
+    </html>
--- index.php
+++ index.php
@@ -1,65 +1,11 @@
-<?php
-
-$nome = "";
-
-$idade = 0;
-
-$mostrar = "";
-
-if ($_SERVER ["REQUEST_METHOD"] == "POST"){
-    
-    $nome = $_POST["nome"];
-    $idade = $_POST["idade"];
-    
-    if ($idade >=18){
-        $mostrar = " De Maior";
-    }
-    else {
-        $mostrar = "De Menor";
-    }
-}
-?>
-
 <!DOCTYPE html>
-<html lang="PT-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <a href="arquivo.php">Atividade x</a>
-    <title>project</title>
+    <title>Document</title>
 </head>
 <body>
-
-
-<form method="POST">
-
-    <input type="text" id="nome" name="nome" placeholder="Digite seu Nome">
-
-    <input type="number" id="idade"  name="idade" placeholder="Digite sua idade">
-
-    <input type="submit" value="Enviar">
-    </form>
-
-    <div class="card">
-
-        
-        <h1>Mostrando Nome, Idade e resultado</h1>
-        
-    
-    <?php if ($mostrar != "") { ?>
-        
-        <h2>
-            o <?= $nome ?> é <?= $mostrar ?> .
-        </h2>
-        
-        <?php } ?>
-        
-        <h1>O meu nome é <?=  $nome?> </h1>
-        <h2>Minha idade é <?=  $idade?> </h2>
-        <p>De acordo com a idade, eu sou <?= $mostrar ?></p>
-
-    </div>
-        
-    </body>
-    </html>
+    <a href="idade.php">verificador de idade</a>
+</body>
+</html>
